9e4a19815d5b298974d1b7686e30cd92cca674ec
Troy Stribling <troy.stribling@gmail.com>
2017-11-05 12:58:54 -0800

    added react examples

---
+++ react/truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      gas: 4712388,
+      from: "0x00a329c0648769A73afAc7F9381E08FB43dBEA72"
+    }
+  }
+};
